4807b8ca6500922c1cf856579ff5939c3cc97df4
Guilherme de Paula <guilhermedepaula.ti@gmail.com>
2022-07-18 20:43:14 -0300

    arrays e metodos

--- eggplant-brownie/MyPlayground.playground/Contents.swift
+++ eggplant-brownie/MyPlayground.playground/Contents.swift
@@ -1,25 +1,15 @@
 import UIKit
 
-let caloria1 = 50.2
-let caloria2 = 100
-let caloria3 = 400
-let caloria4 = 500
+let totalDeCalorias = [50.5, 100]
 
-
-let totalDeCalorias = [50.5, 100, 400, 500, 423, 700]
-print(totalDeCalorias)
-
-// Primeira forma de fazer um laco
-for i in 0...3 {
-    print(totalDeCalorias[i])
+func todasCalorias(totalDeCalorias: [Double]) -> Double {
+    var total: Double = 0
+    
+    for caloria in totalDeCalorias {
+        total += caloria;
+    }
+    
+    return total
 }
-
-//segunda forma de fazer um laco
-for i in 0...totalDeCalorias.count - 1{
-    print(totalDeCalorias[i])
-}
-
-//terceira forma de fazer um laco
-for caloria in totalDeCalorias {
-    print(caloria)
-}
+let total = todasCalorias(totalDeCalorias: totalDeCalorias)
+print(total)
