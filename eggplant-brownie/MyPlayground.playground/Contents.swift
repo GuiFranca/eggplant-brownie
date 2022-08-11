1e22ecf143f5df26fd12867319dd7fef56804e36
Guilherme de Paula <guilhermedepaula.ti@gmail.com>
2022-08-11 16:25:55 -0300

    Metodos e classes

--- eggplant-brownie/MyPlayground.playground/Contents.swift
+++ eggplant-brownie/MyPlayground.playground/Contents.swift
@@ -4,15 +4,44 @@ class Refeicao {
     //Atributos
     var nome: String
     var felicidade: String
+    var itens: Array<Item> = []
     
     //construtor
     init(nome: String, felicidade: String) {
         self.nome = nome
         self.felicidade = felicidade
     }
+    
+    // métodos
+    func totalDeCalorias() -> Double {
+        var total = 0.0
+        
+        for item in itens {
+            total += item.calorias
+        }
+        return total
+    }
 }
 
-let refeicao = Refeicao(nome: "Macarrao", felicidade: "5")
+class Item {
+    var nome: String
+    var calorias: Double
+    
+    init(nome: String, calorias: Double) {
+        self.nome = nome
+        self.calorias = calorias
+    }
+}
 
-print(refeicao.nome)
-print(refeicao.felicidade)
+let arroz = Item(nome: "arroz", calorias: 54.2)
+let feijao = Item(nome: "feijao", calorias: 24.7)
+let contraFile = Item(nome: "contra", calorias: 287.0)
+
+let refeicao = Refeicao(nome: "Macarrao", felicidade: "5")
+refeicao.itens.append(arroz)
+refeicao.itens.append(feijao)
+refeicao.itens.append(contraFile)
+
+print(refeicao.itens.first?.nome)
+print(refeicao.totalDeCalorias())
+
