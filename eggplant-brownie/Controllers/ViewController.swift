ba507b430289b9a48a0ad10e799d0b0b05892bdd
Guilherme de Paula <guilhermedepaula.ti@gmail.com>
2022-11-07 19:50:45 -0300

    tratar exceções com guard ler

--- eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/Controllers/ViewController.swift
@@ -13,18 +13,27 @@ class ViewController: UIViewController {
     @IBOutlet var felicidadeTextField: UITextField?
 
     @IBAction func adicionar(_ sender: Any) {
-        if let nomedaRefeicao = nomeTextField?.text, let felicidadeDaRefeicao = felicidadeTextField?.text {
-            let nome = nomedaRefeicao;
-            if let felicidade = Int(felicidadeDaRefeicao){
-                let refeicao = Refeicao(nome: nome, felicidade: felicidade);
-                
-                print("Comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
-            } else {
-                print("erro ao tentar criar refeição")
-            }
+//        if let nomedaRefeicao = nomeTextField?.text, let felicidadeDaRefeicao = felicidadeTextField?.text {
+//            let nome = nomedaRefeicao;
+//            if let felicidade = Int(felicidadeDaRefeicao){
+//                let refeicao = Refeicao(nome: nome, felicidade: felicidade);
+//
+//                print("Comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
+//            } else {
+//                print("erro ao tentar criar refeição")
+//            }
+//        }
+        
+        guard let nomeDaRefeicao = nomeTextField?.text else {
+            return
         }
         
-       
+        guard let felicidadeDaRefeicao = felicidadeTextField?.text, let felicidade = Int(felicidadeDaRefeicao) else {
+            return
+        }
+        
+        let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade)
+        print("Comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
     }
 }
 
