e3fc91b37292fda4d81c5e5607fa0ee124259c55
Guilherme de Paula <guilhermedepaula.ti@gmail.com>
2022-12-16 10:44:43 -0300

    navigation

---
+++ eggplant-brownie/Controllers/RefeicoesTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RefeicoesTableViewController.swift
+//  eggplant-brownie
+//
+//  Created by Guilherme França de Paula on 15/12/22.
+//
+
+import UIKit
+
+
+class RefeicoesTableViewController: UITableViewController {
+    
+    let refeicoes = [Refeicao(nome: "Macarrão", felicidade: 4),
+                     Refeicao(nome: "Sushi", felicidade: 1),
+                     Refeicao(nome: "Pizza", felicidade: 8)]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return refeicoes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let refeicao = refeicoes[indexPath.row]
+        celula.textLabel?.text = refeicao.nome
+        
+        return celula
+    }
+}
--- eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/Controllers/ViewController.swift
@@ -34,6 +34,8 @@ class ViewController: UIViewController {
         
         let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade)
         print("Comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
+        
+        navigationController?.popViewController(animated: true)
     }
 }
 
